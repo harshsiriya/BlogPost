a2c2ee6b3a61213ab0ecdab6c4c3512a18e4088c
harshsiriya <harshsiriya2@gmail.com>
2025-03-27 13:11:34 +0530

    inital commit

---
+++ src/components/cardlist/Cardlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./cardlist.module.css"
+import Pagination from '../pagination/Pagination'
+
+const Cardlist = () => {
+  return (
+    <div className={styles.container}>Cardlist
+    <Pagination />
+    </div>
+  )
+}
+
+export default Cardlist
